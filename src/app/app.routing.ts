04a83eb1296607c3164e9a686cbd9c6c78c12cd2
David Arias <davidalexarias2@gmail.com>
2018-03-14 22:24:52 +0000

    added cmoments to routing file, to better understand what it is doing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,17 +3,22 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
+ 
+ //Routing imports
 import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
 import { Routes, RouterModule } from '@angular/router';
 
+//import components to be routed to.
 import { AboutComponent } from './about/about.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 
-
+/*This step sets up the routes, matches the url route to 
+the component to be activated upon routing*/
 export const ROUTES: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
-    {path: 'home', component: SearchBarComponent},
+    {path: 'StockSearch', component: SearchBarComponent},
     {path: 'about', component: AboutComponent}
 ];
 
+//This is just part of the routing step, don't NEED to know this per se.
 export const ROUTING: ModuleWithProviders = RouterModule.forRoot(ROUTES);
