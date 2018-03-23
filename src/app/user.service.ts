85f5c7dff374a63cfdf7397e83c4901f43dfe2d7
David Arias <davidalexarias2@gmail.com>
2018-03-23 03:15:08 +0000

    fixed chart colors and fill.

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -9,24 +9,19 @@ export class UserService {
   baseUrl: string = 'http://david-spring-2018-phortonssf.c9users.io:8080/api/appUsers/';
   baseToken: string = '?access_token=';
   baseTokenStocks: string = '/stocks?access_token=';
-  isLoggedIn: any;
+  isLoggedIn: BehaviorSubject<boolean>;
+  
   /*Class Constructor*/
   constructor(private _http: HttpClient) { 
     this.isLoggedIn = new BehaviorSubject(false);
   }
   
-  ///////////////////////////
   //login state-management//
-  
-  
   loginStatus(loginState){
     console.log("hit", loginState)
     this.isLoggedIn.next(loginState)
   }
-  /////////////////////////
-  /////////////////////////
-  
-  
+
   /*Registers User*/  
   registerUser(user){
     return this._http.post(this.baseUrl, user)
